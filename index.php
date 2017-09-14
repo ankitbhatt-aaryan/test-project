d9e9456eceef0b46431b923dcd66c0d68d13d699
ankit-bhatt <ankit.bh@endurance.com>
2017-09-14 20:13:15 +0530

    changes in index.php file

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
 <?php
 //include_once "common.php";
-$path = 'path.pdf';
-$content = 'ankit ';
-
-// save PDF buffer
-file_put_contents($path, $content);
-
-// ensure we don't have any previous output
-if(headers_sent()){
-    exit("PDF stream will be corrupted - there is already output from previous code.");
-}
-
-header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
-header('Pragma: public');
-header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-
-// force download dialog
-header('Content-Type: application/force-download');
-header('Content-Type: application/octet-stream', false);
-header('Content-Type: application/download', false);
-
-// use the Content-Disposition header to supply a recommended filename
-header('Content-Disposition: attachment; filename="'.basename($path).'";');
-header('Content-Transfer-Encoding: binary');
-header('Content-Length: '.filesize($path));
-header('Content-Type: application/pdf', false);
-
-// send binary stream directly into buffer rather than into memory
-readfile($path);
-
-// make sure stream ended
-exit();
 
 phpinfo();
 exit;
